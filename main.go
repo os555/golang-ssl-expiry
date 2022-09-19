ebc0a7742181fe611c62b5e4b040ef13a779c607
Outhai SAIOUDOM <os55555gster@gmail.com>
2022-09-19 11:08:58 +0700

    Create main.go

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"time"
+)
+
+func main() {
+	conn, err := tls.Dial("tcp", "laoitdev.com:443", nil)
+	if err != nil {
+		panic("Server doesn't support SSL certificate err: " + err.Error())
+	}
+
+	err = conn.VerifyHostname("laoitdev.com")
+	if err != nil {
+		panic("Hostname doesn't match with certificate: " + err.Error())
+	}
+	expiry := conn.ConnectionState().PeerCertificates[0].NotAfter
+	fmt.Printf("Issuer: %s\nExpiry: %v\n", conn.ConnectionState().PeerCertificates[0].Issuer, expiry.Format(time.RFC850))
+	fmt.Println(expiry)
+
+	timenow := time.Now().UTC()
+	fmt.Println(timenow)
+
+	remainingTime := expiry.Sub(timenow)
+
+	fmt.Println(remainingTime)
+
+}
